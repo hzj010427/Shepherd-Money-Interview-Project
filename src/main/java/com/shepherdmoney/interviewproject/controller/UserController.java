e723c9fdd0a606927eee5321a5408d913071289b
Zijie <zijieh@andrew.cmu.edu>
2024-05-02 20:27:38 -0400

    feat(UserController): implemented create and delete operations

--- src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
+++ src/main/java/com/shepherdmoney/interviewproject/controller/UserController.java
@@ -1,26 +1,58 @@
 package com.shepherdmoney.interviewproject.controller;
 
+import com.shepherdmoney.interviewproject.model.User;
+import com.shepherdmoney.interviewproject.repository.UserRepository;
 import com.shepherdmoney.interviewproject.vo.request.CreateUserPayload;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
+/**
+ * Controller class responsible for handling web requests related to user operations.
+ * Provides endpoints for creating and deleting users within the system.
+ * 
+ * @author Zijie Huang
+ * @since 05/02/2024
+ */
 @RestController
 public class UserController {
 
-    // TODO: wire in the user repository (~ 1 line)
+    private final UserRepository userRepository;
 
-    @PutMapping("/user")
-    public ResponseEntity<Integer> createUser(@RequestBody CreateUserPayload payload) {
-        // TODO: Create an user entity with information given in the payload, store it in the database
-        //       and return the id of the user in 200 OK response
-        return null;
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
 
+    /**
+     * Creates a new user in the system based on the provided payload.
+     * 
+     * @param payload The data used to create a new user, including name and email.
+     * @return A ResponseEntity containing the ID of the newly created user or an error message.
+     */
+    @PutMapping("/user")
+    public ResponseEntity<Integer> createUser(@RequestBody CreateUserPayload payload) {
+        User user = new User();
+        user.setName(payload.getName());
+        user.setEmail(payload.getEmail());
+        User savedUser = userRepository.save(user);
+        return ResponseEntity.ok(savedUser.getId());
+    }
+
+    /**
+     * Deletes an existing user from the system based on the user ID provided.
+     *
+     * @param userId The ID of the user to delete.
+     * @return A ResponseEntity with a success message if the user is found and deleted, or an error message if not found.
+     */
     @DeleteMapping("/user")
     public ResponseEntity<String> deleteUser(@RequestParam int userId) {
-        // TODO: Return 200 OK if a user with the given ID exists, and the deletion is successful
-        //       Return 400 Bad Request if a user with the ID does not exist
-        //       The response body could be anything you consider appropriate
-        return null;
+        Optional<User> optionalUser = userRepository.findById(userId);
+        if (optionalUser.isPresent()) {
+            userRepository.deleteById(userId);
+            return ResponseEntity.ok("User with ID " + userId + " deleted successfully.");
+        } else {
+            return ResponseEntity.badRequest().body("User with ID " + userId + " does not exist.");
+        }
     }
 }
