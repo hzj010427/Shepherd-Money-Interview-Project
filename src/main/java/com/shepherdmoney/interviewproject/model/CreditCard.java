71ed1a57fbd298dcd591597ad4c6196c8dbd59fd
Zijie <zijieh@andrew.cmu.edu>
2024-05-02 20:23:57 -0400

    feat(CreditCard): added features to insert, retrieve, and update balance

--- src/main/java/com/shepherdmoney/interviewproject/model/CreditCard.java
+++ src/main/java/com/shepherdmoney/interviewproject/model/CreditCard.java
@@ -1,19 +1,24 @@
 package com.shepherdmoney.interviewproject.model;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import java.time.LocalDate;
+import java.util.*;
 
+/**
+ * Entity representing a credit card in the system.
+ * This class includes details such as the issuance bank and card number, and is associated with a User owner.
+ * It handles operations related to the balance history of the card, allowing updates and queries on balances for specific dates.
+ *
+ * @author Zijie Huang
+ * @since 05/02/2024
+ */
 @Entity
 @Getter
 @Setter
-@ToString
 @RequiredArgsConstructor
 public class CreditCard {
 
@@ -25,28 +30,103 @@ public class CreditCard {
 
     private String number;
 
-    // TODO: Credit card's owner. For detailed hint, please see User class
-    // Some field here <> owner;
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User owner;
 
-    // TODO: Credit card's balance history. It is a requirement that the dates in the balanceHistory 
-    //       list must be in chronological order, with the most recent date appearing first in the list. 
-    //       Additionally, the last object in the "list" must have a date value that matches today's date, 
-    //       since it represents the current balance of the credit card.
-    //       This means that if today is 04-16, and the list begin as empty, you receive a payload for 04-13,
-    //       you should fill the list up until 04-16. For example:
-    //       [
-    //         {date: '2023-04-10', balance: 800},
-    //         {date: '2023-04-11', balance: 1000},
-    //         {date: '2023-04-12', balance: 1200},
-    //         {date: '2023-04-13', balance: 1100},
-    //         {date: '2023-04-16', balance: 900},
-    //       ]
-    // ADDITIONAL NOTE: For the balance history, you can use any data structure that you think is appropriate.
-    //        It can be a list, array, map, pq, anything. However, there are some suggestions:
-    //        1. Retrieval of a balance of a single day should be fast
-    //        2. Traversal of the entire balance history should be fast
-    //        3. Insertion of a new balance should be fast
-    //        4. Deletion of a balance should be fast
-    //        5. It is possible that there are gaps in between dates (note the 04-13 and 04-16)
-    //        6. In the condition that there are gaps, retrieval of "closest **previous**" balance date should also be fast. Aka, given 4-15, return 4-13 entry tuple
+    @OneToMany(mappedBy = "creditCard", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BalanceHistory> balanceHistory = new ArrayList<>();
+
+    /**
+     * Inserts a new balance record for a specific date.
+     *
+     * @param date    The date of the balance record.
+     * @param balance The balance amount to record.
+     */
+    public void insertBalance(LocalDate date, double balance) {
+        BalanceHistory history = new BalanceHistory();
+        history.setDate(date);
+        history.setBalance(balance);
+        history.setCreditCard(this);
+        balanceHistory.add(history);
+        sortBalanceHistory();
+    }
+
+    /**
+     * Retrieves the balance for a given date. If no exact date match is found, 
+     * it returns the most recent balance before the date.
+     *
+     * @param date The date to retrieve the balance for.
+     * @return The balance amount or 0.0 if no records exist before the given date.
+     */
+    public double getBalance(LocalDate date) {
+        for (BalanceHistory history : balanceHistory) {
+            if (history.getDate().isEqual(date)) {
+                return history.getBalance();
+            }
+        }
+
+        for (BalanceHistory history : balanceHistory) {
+            if (!history.getDate().isAfter(date)) {
+                return history.getBalance();
+            }
+        }
+
+        return 0.0;
+    }
+
+    /**
+     * Updates the balance for a specific date or inserts a new record 
+     * if no existing record is found for that date.
+     *
+     * @param date    The date for which the balance needs to be updated.
+     * @param balance The new balance amount.
+     */
+    public void updateBalance(LocalDate date, double balance) {
+        boolean found = false;
+        for (BalanceHistory history : balanceHistory) {
+            if (history.getDate().isEqual(date)) {
+                history.setBalance(balance);
+                history.setCreditCard(this);
+                found = true;
+                break;
+            }
+        }
+
+        // If record for the specified date doesn't exist, insert a new record
+        if (!found) {
+            this.insertBalance(date, balance);
+        }
+    }
+
+    /**
+     * Retrieves the balance history for the credit card.
+     * 
+     * @return A string representation of the balance history.
+     */
+    public String getBalanceHistoryString() {
+        StringBuilder sb = new StringBuilder();
+        for (BalanceHistory history : balanceHistory) {
+            sb.append(history.getDate()).append(": ").append(history.getBalance()).append("\n");
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Sorts the balance history in reverse chronological order.
+     */
+    private void sortBalanceHistory() {
+        balanceHistory.sort(Comparator.comparing(BalanceHistory::getDate).reversed());
+    }
+
+    @Override 
+    public String toString() {
+        return "CreditCard{" +
+                "id=" + id +
+                ", issuanceBank='" + issuanceBank + '\'' +
+                ", number='" + number + '\'' +
+                ", owner=" + owner +
+                ", balanceHistory=" + balanceHistory +
+                '}';
+    }
 }
