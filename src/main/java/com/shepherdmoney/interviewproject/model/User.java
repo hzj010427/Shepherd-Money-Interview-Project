d538a1457aef67f71a263b450a2b94e6b768aaf2
Zijie <zijieh@andrew.cmu.edu>
2024-05-02 20:24:43 -0400

    feat(User): added features to add and remove credit card

--- src/main/java/com/shepherdmoney/interviewproject/model/User.java
+++ src/main/java/com/shepherdmoney/interviewproject/model/User.java
@@ -6,6 +6,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Entity representing a user in the system. 
+ * This class is mapped to the "MyUser" table in the database and includes basic user details such as name and email.
+ * Users can own multiple credit cards, which are managed through a one-to-many relationship.
+ *
+ * @author Zijie Huang
+ * @since 05/02/2024
+ */
 @Entity
 @Getter
 @Setter
@@ -22,7 +33,28 @@ public class User {
 
     private String email;
 
-    // TODO: User's credit card
-    // HINT: A user can have one or more, or none at all. We want to be able to query credit cards by user
-    //       and user by a credit card.
+    @OneToMany(mappedBy = "owner", fetch = FetchType.LAZY)
+    private List<CreditCard> creditCards = new ArrayList<>();
+
+    /**
+     * Adds a credit card to this user's list of cards.
+     * Sets the owner of the credit card to this user.
+     *
+     * @param creditCard The credit card to be added.
+     */
+    public void addCreditCard(CreditCard creditCard) {
+        creditCards.add(creditCard);
+        creditCard.setOwner(this);
+    }
+
+    /**
+     * Removes a credit card from this user's list of cards.
+     * Sets the owner of the credit card to null.
+     *
+     * @param creditCard The credit card to be removed.
+     */
+    public void removeCreditCard(CreditCard creditCard) {
+        creditCards.remove(creditCard);
+        creditCard.setOwner(null);
+    }
 }
